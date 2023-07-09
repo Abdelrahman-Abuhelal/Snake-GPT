f8ea839e2c281936869ece6634ae9fb5d8569b1e
abdelrahman <abd.hilal14@gmail.com>
2023-07-09 20:31:38 +0200

    generate same content id for the same content

--- extension/background.mjs
+++ extension/background.mjs
@@ -12,15 +12,33 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         },
         function (result) {
           var content = result[0].result;
-          var contentId =generate_uuidv4(); // Generate a unique id for the content
-          sendResponse({ contentData:content, contentId: contentId });
-          sendContentToApi(contentId, content);
+          generateOrRetrieveUniqueId(content, function(contentId) {
+            sendResponse({ contentData: content, contentId: contentId });
+            sendContentToApi(contentId, content);
+          });
         }
       );
     });
   }
   return true;
 });
+
+function generateOrRetrieveUniqueId(content, callback) {
+  chrome.storage.local.get(content, function (data) {
+    var storedContent = data[content]; // Check if content already has an ID stored
+    if (storedContent) {
+      callback(storedContent); // Return the existing ID
+    } else {
+      var contentId = generate_uuidv4(); // Generate a new ID
+      var newData = {};
+      newData[content] = contentId;
+      chrome.storage.local.set(newData, function () {
+        callback(contentId); // Store the ID and return it
+      });
+    }
+  });
+}
+
 
 
 function generate_uuidv4() {
